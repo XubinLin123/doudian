e49804b39900ec23c303a94f6daf5af99f1f3218
XubinLin123 <48861258+XubinLin123@users.noreply.github.com>
2026-07-11 10:24:43 +0800

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -6,10 +6,10 @@
 	  // 或者用 * 允许所有来源（测试时用）
 	  res.setHeader('Access-Control-Allow-Origin', '*');
 	  //res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-	 // res.setHeader(
-	    'Access-Control-Allow-Headers',
-	    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-	  );
+	  // res.setHeader(
+	  //  'Access-Control-Allow-Headers',
+	  // 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+	  //  );
 	  
 	  // 处理预检请求
 	  if (req.method === 'OPTIONS') {
